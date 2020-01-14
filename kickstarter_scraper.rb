6bb36103c15d71049fcb2e3dc2e99fbd551a8404
Nick Solonyy <nicksolony@gmail.com>
2020-01-14 05:00:41 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -18,6 +18,7 @@ def create_project_hash
  
   kickstarter.css("li.project.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text
+  binding.pry  
     projects[title.to_sym] = {
       :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
       :description => project.css("p.bbcard_blurb").text,
@@ -28,5 +29,5 @@ def create_project_hash
  
   # return the projects hash
   projects
-  binding.pry
+  
 end
